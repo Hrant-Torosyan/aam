d7fe3a620c821229f847e45998bf63b7a11a3705
Liana Mkrtchyan <liana.mkrtchyan@cookies.games>
2025-02-18 01:08:12 +0400

    slider

--- src/components/access/Main/ProductInfo/SimilarProjects/SimilarSlider.jsx
+++ src/components/access/Main/ProductInfo/SimilarProjects/SimilarSlider.jsx
@@ -1,124 +1,22 @@
-import React, { useContext } from "react";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { useRef, useContext } from "react";
+import "swiper/swiper-bundle.css";
 import "./SimilarSlider.scss";
-import "./SimilarSliderResponsive.scss";
-import Slider from "react-slick";
-import Product from "../../Product/Product";
-import { MainContext } from "../../../../../app/App";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 import leftArrow from "../../../../svg/leftArrow.svg";
 import rightArrow from "../../../../svg/rightArrow.svg";
+import Product from "../../Product/Product";
+import { MainContext } from "../../../../../app/App";
 
-const CustomPrevArrow = ({ onClick }) => (
-	<button className="customArrow prevArrow" onClick={onClick}>
-		<img src={leftArrow} alt="Previous" />
-	</button>
-);
-const CustomNextArrow = ({ onClick }) => (
-	<button className="customArrow nextArrow" onClick={onClick}>
-		<img src={rightArrow} alt="Next" />
-	</button>
-);
-const settings = {
-	dots: false,
-	infinite: false,
-	speed: 500,
-	slidesToShow: 4,
-	slidesToScroll: 2,
-	arrows: true,
-	prevArrow: <CustomPrevArrow />,
-	nextArrow: <CustomNextArrow />,
-	centerMode: false,
-	variableWidth: true,
-	responsive: [
-		{
-			breakpoint: 1200,
-			settings: {
-				arrows: false,
-				slidesToShow: 4,
-				slidesToScroll: 2,
-			},
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				arrows: false,
-				slidesToShow: 3,
-				slidesToScroll: 1,
-				centerMode: false,
-				variableWidth: false,
-			},
-		},
-		{
-			breakpoint: 670,
-			settings: {
-				arrows: false,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				centerMode: false,
-				variableWidth: false,
-			},
-		},
-		{
-			breakpoint: 576,
-			settings: {
-				arrows: false,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				centerMode: false,
-				variableWidth: false,
-			},
-		},
-		{
-			breakpoint: 470,
-			settings: {
-				arrows: false,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				centerMode: false,
-				variableWidth: false,
-			},
-		},
-		{
-			breakpoint: 410,
-			settings: {
-				arrows: false,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				centerMode: false,
-				variableWidth: false,
-			},
-		},
-		{
-			breakpoint: 375,
-			settings: {
-				arrows: false,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				centerMode: false,
-				variableWidth: false,
-			},
-		},
-		{
-			breakpoint: 360,
-			settings: {
-				arrows: false,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				centerMode: false,
-				variableWidth: false,
-			},
-		},
-	],
-};
-const SimilarSlider = ({ products = [], info, prodId, setProdId, handleImageError }) => {
+const SimilarSlider = ({ products = [], prodId, setProdId, handleImageError }) => {
 	const mainCntx = useContext(MainContext);
+	const prevRef = useRef(null);
+	const nextRef = useRef(null);
+
 	const handleClick = (id) => {
 		setProdId(id);
 		window.scrollTo({ top: 0 });
 	};
-	// const filteredProducts = products.filter((p) => p.projectId !== prodId);
 
 	const filteredProducts = [
 		{
@@ -215,31 +113,49 @@ const SimilarSlider = ({ products = [], info, prodId, setProdId, handleImageErro
 	];
 
 	if (!filteredProducts.length) return null;
+
 	return (
 		<div className="similarSliderWrapper">
 			<div className="profileSlider">
 				<h1 className="sliderTitle">Похожие проекты</h1>
-				<Slider {...settings}>
-					{filteredProducts.length > 0 ? (
-						filteredProducts.map((prod, index) => (
-							<div key={index} className="slideItem">
-								<Product
-									fullWidth={true}
-									key={index}
-									prod={prod}
-									info={info}
-									setProdId={handleClick}
-									handleImageError={handleImageError}
-									setHiddenHeader={mainCntx.setHiddenHeader}
-								/>
-							</div>
-						))
-					) : (
-						<div className="noProductsMessage">
-							<p>_</p>
-						</div>
-					)}
-				</Slider>
+				<Swiper
+					modules={[Navigation]}
+					spaceBetween={16}
+					slidesPerView='auto'
+					breakpoints={{
+						640: { slidesPerView: 1 },
+						768: { slidesPerView: 2 },
+						1024: { slidesPerView: 3 },
+						1280: { slidesPerView: 4 },
+					}}
+					navigation={{
+						prevEl: prevRef.current,
+						nextEl: nextRef.current,
+					}}
+					onBeforeInit={(swiper) => {
+						swiper.params.navigation.prevEl = prevRef.current;
+						swiper.params.navigation.nextEl = nextRef.current;
+					}}
+					loop={false}
+				>
+					{filteredProducts.map((prod, index) => (
+						<SwiperSlide key={prod.projectId || index} style={{ width: "280px" }}>
+							<Product
+								fullWidth={true}
+								prod={prod}
+								handleImageError={handleImageError}
+								setHiddenHeader={mainCntx.setHiddenHeader}
+								setProdId={handleClick}
+							/>
+						</SwiperSlide>
+					))}
+				</Swiper>
+				<button ref={prevRef} className="customArrow prevArrow">
+					<img src={leftArrow} alt="Previous" />
+				</button>
+				<button ref={nextRef} className="customArrow nextArrow">
+					<img src={rightArrow} alt="Next" />
+				</button>
 			</div>
 		</div>
 	);
